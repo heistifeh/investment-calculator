eb7d113ca0a332a0f5a92de13f0cf2280b96c9a3
heistifeh <teefeh26@gmail.com>
2025-04-09 00:34:02 +0100

    finished first project!

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import Header from "./components/Header";
 import Input from "./components/Input";
 import { useState } from "react";
@@ -6,20 +5,21 @@ import Table from "./components/Table";
 import { use } from "react";
 
 function App() {
-  
-
   const [userInput, setUserInput] = useState({});
 
   const handleReceivedInput = (input) => {
     setUserInput(input); //receive input from child component and set it to state instantly
-  }
+  };
   console.log(userInput);
-  
+  if (userInput.duration < 0) {
+    alert("Please enter a valid duration");
+    userInput.duration = 0;
+  }
   return (
     <>
       <Header title="Investment Calculator" />
-      <Input sendUserInputToParent={handleReceivedInput}/>
-      <Table inputData={userInput}/>
+      <Input sendUserInputToParent={handleReceivedInput} />
+      <Table inputData={userInput} />
     </>
   );
 }
